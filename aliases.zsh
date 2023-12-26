d5d6b4b01568b0dcc55c2bb89a4551bb4f1fdb96
Plump Albert <plumpalbert@gmail.com>
2023-12-27 01:04:06 +0300

    display only distro names

--- aliases.zsh
+++ aliases.zsh
@@ -11,12 +11,17 @@ alias v='${EDITOR:-vim}'
 alias sv='sudo -A ${EDITOR:-vim}'
 
 if [[ $commands[nvim] && $commands[fzf] && $commands[fd] ]]; then
+	alias vim='nvim'
 	function vv() {
-		local config=$(fd --max-depth 1 --glob 'nvim*' "${XDG_CONFIG_HOME:-$HOME/.config}" | fzf --prompt="Neovim Configs > " --height=~50% --layout=reverse --border --exit-0)
+		local config=$(
+			fd --max-depth 1 --glob 'nvim*' "${XDG_CONFIG_HOME:-$HOME/.config}" \
+				| sed 's|.*/\(.*\)/$|\1|' \
+				| fzf --prompt="Neovim Configs > " --height=~50% --layout=reverse --border --exit-0 \
+		)
 
 		[[ -z $config ]] && echo "No config was selected" && return
 
-		NVIM_APPNAME=$(basename "${config}") nvim
+		NVIM_APPNAME=$(basename "${config}") nvim $@
 	}
 fi
 

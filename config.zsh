200a5a9318b7a0a4624bce12a6565beb500eb6b9
Plump Albert <plumpalbert@gmail.com>
2023-12-26 13:03:24 +0300

    set ZSH options

--- config.zsh
+++ config.zsh
@@ -12,4 +12,11 @@ bindkey '^H' backward-kill-word
 bindkey '5~' kill-word
 bindkey '^X^E' edit-command-line
 
-setopt promptsubst auto_cd auto_pushd extended_glob hist_ignore_dups inc_append_history
+setopt PROMPTSUBST
+setopt AUTO_CD
+setopt AUTO_PUSHD
+setopt PUSHD_IGNORE_DUPS
+setopt PUSHD_SILENT
+setopt EXTENDED_GLOB
+setopt SHARE_HISTORY
+setopt HIST_SAVE_NO_DUPS

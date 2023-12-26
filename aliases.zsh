b88c7acc810c55dd7a776278f58866c20da6b41d
Plump Albert <plumpalbert@gmail.com>
2023-12-26 13:03:29 +0300

    updated aliases

--- aliases.zsh
+++ aliases.zsh
@@ -1,14 +1,14 @@
 #!/bin/zsh
 
+alias sudo='sudo ' # enable expansion for `sudo` commands
+
 alias zcr='source ${ZDOTDIR:-$HOME}/.zshrc'
 alias zce='${EDITOR:-vim} ${ZDOTDIR:-$HOME}/.zshrc'
 
 alias lg=lazygit
 
-if [ $commands[nvim] ]; then
-	alias vim=nvim
-	alias v=nvim
-fi
+alias v='${EDITOR:-vim}'
+alias sv='sudo -A ${EDITOR:-vim}'
 
 if [ $commands[tmuxp] ]; then
 	alias txf='tmuxp freeze'
